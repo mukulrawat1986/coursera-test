2aeb1296611a7acc626f4f34501102d01ccfdfe8
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-17 17:52:39 +0530

    done some object creation

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,10 +1,21 @@
-function orderChickenWith(sideDish){
-	// if (sideDish == undefined){
-	// 	sideDish = "whatever!";
-	// }
-	sideDish = sideDish || "whatever!";
-	console.log("Chicken with " + sideDish);
-}
+// object creation
+var company = new Object();
+company.name = "Facebook";
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+company.ceo.favColor = "blue";
+console.log(company);
+console.log("Company CEO name is: " 
+	+ company.ceo.firstName);
 
-orderChickenWith("noodles");
-orderChickenWith(); // Chicken with undefined
+console.log(company["name"]);
+
+company["stock of company"] = 110;
+
+console.log(company);
+
+var temp = "this is a big property name";
+company[temp] = 110;
+
+console.log("big property name is " 
+	+ company[temp]);
