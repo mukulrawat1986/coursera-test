b9a54b97aa5f17ce69e8a1a59436a4c493d412ff
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-17 17:57:46 +0530

    object literal method done

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,21 +1,13 @@
-// object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "blue";
-console.log(company);
-console.log("Company CEO name is: " 
-	+ company.ceo.firstName);
+// better way: object literal
+var facebook = {
+	name: "Facebook",
+	ceo: {
+		firstName: "Mark",
+		favColor: "blue"
+	},
+	"stock of company": 110
+};
 
-console.log(company["name"]);
-
-company["stock of company"] = 110;
-
-console.log(company);
-
-var temp = "this is a big property name";
-company[temp] = 110;
-
-console.log("big property name is " 
-	+ company[temp]);
+console.log(facebook);
+console.log(facebook.ceo.firstName);
+console.log(facebook["stock of company"]);
