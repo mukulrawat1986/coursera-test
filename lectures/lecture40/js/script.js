d53b223e18ee4767b6c9810cdafb2167aea42389
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-17 19:39:15 +0530

    functions as objects done

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,13 +1,45 @@
-// better way: object literal
-var facebook = {
-	name: "Facebook",
-	ceo: {
-		firstName: "Mark",
-		favColor: "blue"
-	},
-	"stock of company": 110
-};
+// functions are first class data types
+// functions are objects
+// whatever you can do with a variable or an 
+// object can be done with the functions.
+function multiply(x, y){
+	return x * y;
+}
 
-console.log(facebook);
-console.log(facebook.ceo.firstName);
-console.log(facebook["stock of company"]);
+console.log(multiply(5, 3));
+multiply.version = "v.1.0.0";
+console.log(multiply);
+
+// every object has a inherent method to it called
+// toString() which helps in printing out the object
+// as a string
+console.log(multiply.toString);
+console.log(multiply.toString());
+console.log(multiply.version);
+
+// function factory
+function makeMultiplier(multiplier){
+	var myFunc = function(x){
+		return multiplier * x;
+	};
+
+	return myFunc;
+}
+
+// 
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+
+// passing function as arguments
+function doOperationOn(x, operation){
+	return operation(x);
+}
+
+var result = doOperationOn(5, multiplyBy3);
+console.log(result);
+
+result = doOperationOn(100, doubleAll);
+console.log(result);
