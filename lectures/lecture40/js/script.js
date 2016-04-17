9d07ffde45066e2fc9afd79fd218070e50175ab7
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-17 17:05:54 +0530

    common language constructs

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,14 +1,41 @@
-// should be undefined
-var x;
-console.log(x);
+// ***** String concatenation
+var string = "Hello";
+string += " World";
+console.log(string + "!");
 
-if (x == undefined){
-	console.log("x is undefined");
+// Regular math operators
+console.log(( 5 * 4) / 3);
+console.log(undefined / 5);
+function test1(a){
+	console.log(a/5);
+}
+test1();
+
+// Equality
+var x = 4, y = 4;
+if (x == y){
+	console.log("x=4 equals to y=4");
 }
 
-x = 5;
-if (x == undefined){
-	console.log("x is undefined");
+x = "4";
+if(x == y){
+	console.log("x='4' is equal to y=4");
+}
+// strict equality
+if (x===y){
+	console.log("Strict: x='4' is equal to y=4");
 }else{
-	console.log("x has been defined");
+	console.log("Strict: x='4' is not equal to y=4");
+}
+
+// if statement (all false)
+if (false || null || undefined || "" || 0 || NaN){
+	console.log("This line won't ever execute");
+}else{
+	console.log("All false");
+}
+
+// if statement ( all true)
+if (true && "hello" && 1 && -1 && "false"){
+	console.log("All true");
 }
