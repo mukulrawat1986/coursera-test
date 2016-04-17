3b45890f7536a2844cd893d096c64e79b148739e
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-17 17:38:16 +0530

    default value handling done

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,67 +1,10 @@
-// ***** String concatenation
-var string = "Hello";
-string += " World";
-console.log(string + "!");
-
-// Regular math operators
-console.log(( 5 * 4) / 3);
-console.log(undefined / 5);
-function test1(a){
-	console.log(a/5);
-}
-test1();
-
-// Equality
-var x = 4, y = 4;
-if (x == y){
-	console.log("x=4 equals to y=4");
+function orderChickenWith(sideDish){
+	// if (sideDish == undefined){
+	// 	sideDish = "whatever!";
+	// }
+	sideDish = sideDish || "whatever!";
+	console.log("Chicken with " + sideDish);
 }
 
-x = "4";
-if(x == y){
-	console.log("x='4' is equal to y=4");
-}
-// strict equality
-if (x===y){
-	console.log("Strict: x='4' is equal to y=4");
-}else{
-	console.log("Strict: x='4' is not equal to y=4");
-}
-
-// if statement (all false)
-if (false || null || undefined || "" || 0 || NaN){
-	console.log("This line won't ever execute");
-}else{
-	console.log("All false");
-}
-
-// if statement ( all true)
-if (true && "hello" && 1 && -1 && "false"){
-	console.log("All true");
-}
-
-// Best practice for {} style
-function a()
-{
-	return
-	{
-		name: "Yaakov"
-	};
-}
-
-function b(){
-	return{
-		name: "Yaakov"
-	};
-}
-
-console.log(a());
-console.log(b());
-
-// for loop
-var sum = 0;
-for (var i=0; i <10; i++){
-	console.log(i);
-	sum += i;
-}
-console.log("Sum of first 9 numbers is " + sum);
+orderChickenWith("noodles");
+orderChickenWith(); // Chicken with undefined
