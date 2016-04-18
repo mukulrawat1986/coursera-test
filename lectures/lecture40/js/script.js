5bc5baec3266f2ca3a1851af367ccb6e26396862
Mukul Rawat <mukulsrawat@gmail.com>
2016-04-18 21:46:43 +0530

    added an example of this

--- lectures/lecture40/js/script.js
+++ lectures/lecture40/js/script.js
@@ -1,54 +1,9 @@
-// copy by reference vs by value
-// var a = 7;
-// var b = a;
-// console.log("a: " + a);
-// console.log("b: " + b);
+// this points to the global window object
 
-// b = 5;
-// console.log("after b updates");
-// console.log("a: " + a);
-// console.log("b: " + b);
-
-// pass by reference
-var a = {x: 7};
-var b = a;
-console.log(a);
-console.log(b);
-
-b.x = 5;
-console.log("after b.x update");
-console.log(a);
-console.log(b);
-
-// pass by reference vs by value
-function changePrimitive(primValue){
-	console.log("in changePrimitive...");
-	console.log("before: ");
-	console.log(primValue);
-
-	primValue = 5;
-	console.log("after: ");
-	console.log(primValue);
+function test(){
+	console.log(this);
+	// added a property to the global window object
+	this.myName = "Mukul";
 }
-
-var value = 7;
-changePrimitive(value);
-console.log("after changePrimitive, orig value:");
-console.log(value);
-
-function changeObject(objValue){
-	console.log("in changeObject....");
-	console.log("Before: ");
-	console.log(objValue);
-
-	objValue.x = 5;
-	console.log("after: ");
-	console.log(objValue);
-}
-
-value1 = {
-	x: 7
-};
-changeObject(value1);
-console.log("after changeObject, orig value:");
-console.log(value1);
+test();
+console.log(window.myName);
